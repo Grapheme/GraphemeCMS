3d65afdc788a7540f48c044f9e700f4c1ff9beed
vkharseev <vkharseev@gmail.com>
2014-07-23 16:41:17 +0400

    Почистил шаблоны и изменил инструкцию

--- app/views/templates/default/head.blade.php
+++ app/views/templates/default/head.blade.php
@@ -2,6 +2,4 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>{{{(isset($page_title))?$page_title:Config::get('app.default_page_title')}}}</title>
 <meta name="description" content="{{{(isset($page_description))?$page_description:''}}}">
-{{ HTML::style('theme/css/normalize.css') }}
-{{ HTML::style('theme/css/main.css') }}
 {{ HTML::script('js/vendor/modernizr-2.6.2.min.js') }}
