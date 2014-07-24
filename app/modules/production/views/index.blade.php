12d864d215eb7ec61f20b367f002eb1217b72a0f
vkharseev <vkharseev@gmail.com>
2014-07-24 18:42:00 +0400

    Обновил модули News, Events, Reviews
    Нет возможеости просотреть странцу новости, события или отзыва

--- app/modules/production/views/index.blade.php
+++ app/modules/production/views/index.blade.php
@@ -1,5 +1,4 @@
 <?php
-//<a href="{{-- link::to('catalog/'.$product['link']) --}}">{{-- $product['title'] --}}</a>
 if($products = Product::orderBy('title')->with('images')->get()):
     $products = $products->toArray();
 endif;
@@ -13,7 +12,7 @@ endif;
                 <a class="product-photo" href="{{ link::to('catalog/'.$product['link']) }}" style="background-image: url(uploads/galleries/{{ $product['images']['name'] }});"></a>
                 <div class="product-info">
                     <div class="block-title">
-                        <a href="{{ $product['link'] }}">{{ $product['title'] }}</a>
+                        <a href="{{ link::to('catalog/'.$product['link']) }}">{{ $product['title'] }}</a>
                     </div>
                     <div class="us-text">
                         {{ $product['short'] }}
